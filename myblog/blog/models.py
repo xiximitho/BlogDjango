0c21f986e0e19cde7ef6810bd886a3128ce59655
Mattes <mattesfelipe@gmail.com>
2020-10-15 20:10:19 -0300

    Criado gerenciador de modelo, views e templates html e css. (app rodando ja)

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
+
+#criando um gerenciador de madelo do banco
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')  #funcao que retorna os posts publicados.
+
 
 class Post(models.Model):  #tipos de status de cada Post no blog
     STATUS_CHOICES = (('draft','Draft'),
@@ -16,12 +23,22 @@ class Post(models.Model):  #tipos de status de cada Post no blog
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES)
 
+    objects = models.Manager() #Gerenciador de modelo Default
+    published = PublishedManager() #Gerenciador personalizado que retorna os posts publicados.
+
+    def get_absolute_url(self): #ligar postagens especificas.
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug])
 
     class Meta:
         ordering = ('-publish',) #serao ordeenados pela data de publicacao
 
     def __str__(self):
         return self.title
+
+
 
 class Evento(models.Model):
     STATUS_EVENTO = (('encerrado','Encerrado'),
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 # Create your views here.
+
+def post_list(request):
+    posts = Post.published.all() #Realizando a chamada da funcao feita no Models.py para a View
+    return render(request, 'blog/post/list.html',{'posts': posts}) #FUNCAO RENDER PARA PLOTAR A VIEW, recebe como parametro request, path do template e variaveis do contexto para renderizar
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                             status='published',
+                             publish_year=year,
+                             publish_month=month,
+                             publish_day=day) #SE o objeto existir ele retorna ao objeto post, caso contrario exibe 404
+
+    return render(request, 'blog/post/detail.html', {'post': post})
+
