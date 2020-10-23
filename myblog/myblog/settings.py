df840dbfa005d2eba7de42b51aa7b59aff8e58c0
Mattes <mattesfelipe@gmail.com>
2020-10-23 20:37:10 -0300

    Criado envio de emails (sem SMTP, apenas terminal por enquanto)

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#ENVIO DE EMAIL>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
